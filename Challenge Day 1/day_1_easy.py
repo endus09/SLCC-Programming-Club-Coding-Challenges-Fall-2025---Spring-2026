bcddbb9b9093a01146e618628b8971955acecd82
endus <137748797+endus09@users.noreply.github.com>
2025-09-08 20:48:21 -0600

    Created Day 1 Easy Challenge

---
+++ Challenge Day 1/day_1_easy.py
@@ -0,0 +1,34 @@
+# Even or Odd
+# By Markus Gulla
+
+# Hello everyone!!!! Welcome to the first programming challenge of the club!
+# In today's challenge, you will be creating a function that receives an integer as an argument and returns 
+# "Even" for even numbers and "Odd" for odd numbers. 
+
+def even_or_odd(x):
+    # WRITE YOUR CODE HERE
+
+    return 0
+
+
+
+t1 = 13
+t2 = 0
+t3 = 2
+t4 = 57
+t5 = 578
+
+test_one = even_or_odd(t1)
+print(t1,"is ODD --- YOUR CODE RETURNED:",test_one)
+
+test_two = even_or_odd(t2)
+print(t2,"is EVEN --- YOUR CODE RETURNED:",test_two)
+
+test_three = even_or_odd(t3)
+print(t3,"is EVEN --- YOUR CODE RETURNED:",test_three)
+
+test_four = even_or_odd(t4)
+print(t4,"is ODD --- YOUR CODE RETURNED:",test_four)
+
+test_five = even_or_odd(t5)
+print(t5,"is EVEN --- YOUR CODE RETURNED:",test_five)

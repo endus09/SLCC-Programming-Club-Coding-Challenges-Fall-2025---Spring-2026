e4fc3ee90310206814349bc4b52ae1d2a8b7b2e4
WhisperKnight <61612853+Bryson-C@users.noreply.github.com>
2025-10-12 22:18:03 -0600

    Create ColorRGBProduct.py
    Created Python Challenges For October 13th, 2025

---
+++ 10-13-25/ColorRGBProduct.py
@@ -0,0 +1,56 @@
+
+# CHALLENGE CODE
+# {"color":rgb_index}
+color_pallete = { "red": 2, "orange": 3, "yellow": 5, "green": 7, "blue": 9, "indigo": 10, "violet": 12 }
+
+# [ {r, g, b}, ...]
+rgb_values = [
+	[0,0,0],
+	[90, 56, 28],
+	[240, 25, 17],
+	[240, 147, 17],
+	[100, 90, 50],
+	[245, 255, 50],
+	[200, 200, 200],
+	[72, 255, 50],
+	[60, 58, 40],
+	[50, 64, 255],
+	[109, 61, 255],
+	[0,0,200],
+	[132, 9, 255],
+	[100, 50, 20],
+	[30, 190, 80]
+]
+
+# Given A Color's Name, If It Exists: Return It's RGB Index, And The Product Of Its R, G, And B Values: 
+# [rgb_index, product of r, g, and b]
+# If The Color Does Not Exist In The Pallete Return An Empty List
+#
+# WRITE YOUR CODE IN HERE
+def getColorInfo(color_name):
+	return []
+
+
+# TESTS
+def test(color_name, expected):
+	print(f'\n = = = Test {color_name} = = = ')
+	print("Got:      ", getColorInfo(color_name))
+	print("Expected: ", expected)
+	if (getColorInfo(color_name) == expected):
+		print("Test Passed")
+	else:
+		print("Test Failed")
+	print(f' = = = Test {color_name} = = = \n')
+
+
+test("blue", [9, 816000])
+test("red", [2, 102000])
+test("purple", [])
+test("yellow", [5, 3123750])
+test("purple", [])
+test("indigo", [10, 1695495])
+test("lime", [])
+test("green", [7, 918000])
+test("white", [])
+test("orange", [3, 599760])
+

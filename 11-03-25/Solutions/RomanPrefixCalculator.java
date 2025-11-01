59002e7d2ac2d3da52aea1036dfca107b6b7febd
WhisperKnight <61612853+Bryson-C@users.noreply.github.com>
2025-10-31 23:13:29 -0600

    Create RomanPrefixCalculator.java
    Added Java Solution

---
+++ 11-03-25/Solutions/RomanPrefixCalculator.java
@@ -0,0 +1,124 @@
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+
+/**
+ * Welcome To Another Week Of Coding Challenges!
+ * 
+ * This Week We Are Doing What I Call "Roman Prefix Calculator": A Mix Between Roman Numerals And A Prefix Notation Calculator
+ * First Step Is Figuring Out Roman Numerals:
+* 
+* 	I = 1
+* 	V = 5
+* 	X = 10
+* 	L = 50
+* 	C = 100
+* 
+* 	III = 3
+* 	XII = 12
+* 	IX = 9
+* 	XLV = 45 You Can Think Of As (10 - 50 + 5)
+* 	IIXX = 18 You Can Think Of As (2 - 20)
+* 
+* (10 - 50, Is Typically Negative 40, But For Sake Of Demonstration, The Values Are Where They Appear In The Roman Numeral (i.e. 10 Came Before 50),
+* Since We Aren't Doing Negatives, You May Also Simply Return An Absolute Value, If It Makes More Sense)
+* 
+ * The Next Stage Is Prefix Notation:
+ * 
+ * 	In Prefix Notation, The Operation Comes Before The Left And Right Values: 
+ * 		(+ 1 1)
+ * 	This Is Exactly The Same Operation As
+ * 		1 + 1
+ * 
+ * 	You Can Also Chain Operations:
+ * 		(* (+ 2 3) 5)
+ * 	Which Is The Same As:
+ * 		(2 + 3) * 5
+ * 
+ * Notes:
+ * 	- Input Numerals Will Always Be Less Than Or Equal 100, But May Contain The C Character
+ *  - IIII And IV Are The Same And Are Equally Valid Historically
+ * 	- Parenthesis Will Not Be Used For The Test Inputs, 
+ * 			So You Will Get "+ I I" As An Example Rather Than "(+ I I)"
+ * 			And "* * III III III" Instead Of "(* (* III III) III)"
+ * 			They Evaluate The Same, But Just Keep This In Mind
+ * 
+ * @author Bryson Crader
+ */
+
+public class RomanPrefixCalculator {
+
+
+	private static int RomanNumeralToInt(String numeral) {
+		List<Character> numeralChr = Arrays.asList('I', 'V', 'X', 'L', 'C');
+		int numeralVals[] = {0, 1, 5, 10, 50, 100};
+
+		int sum = 0;
+		int buffer = 0;
+		for (int i = 0; i < numeral.length(); i++) {
+			int chrVal = numeralVals[numeralChr.indexOf(numeral.charAt(i))+1];
+			
+			if (chrVal > buffer && i > 0) {
+				sum += (chrVal - buffer);
+				buffer = 0;
+				continue;
+			}
+			buffer += chrVal;
+		}
+		sum += buffer;
+
+		return sum;
+	}
+
+	public static int EvaluateRomanPrefixNotation(String str) {
+		Stack<Integer> numbers = new Stack<>();
+		Stack<Character> operations = new Stack<>();
+
+		String split[] = str.split(" ");
+		for (int i = 0; i < split.length; i++) {
+			if (split[i].charAt(0) == '+' || split[i].charAt(0) == '-' || split[i].charAt(0) == '*' || split[i].charAt(0) == '/')
+				operations.push(split[i].charAt(0));
+			else
+				numbers.push(RomanNumeralToInt(split[i]));
+
+			if (numbers.size() >= 2 && !operations.empty()) {
+
+
+				int a = numbers.pop(), b = numbers.pop(), c = 0;
+				char op =  operations.pop();
+				switch (op) {
+					case '+' -> c = a + b;
+					case '-' -> c = a - b;
+					case '*' -> c = a * b;
+					case '/' -> c = a / b;
+				}
+				numbers.push(c);
+			}
+		}
+
+		return Math.abs(numbers.firstElement());
+	}
+
+	public static void main(String[] args) {
+		RunTest("+ I I", 2);
+		RunTest("* + I I II", 4);
+		RunTest("* - I I II", 0);
+		RunTest("/ L C", 2);
+		RunTest("- XLV X", 35);
+		RunTest("+ XXII IV", 26);
+		RunTest("* * * I II III IIII", 24);
+		RunTest("+ + VII II XC", 99);
+	}	
+
+	public static void RunTest(String input, int expected) {
+		int result = EvaluateRomanPrefixNotation(input);
+		System.out.printf("\n= = = Test %s = = =\n", result == expected ? "Passed" : "Failed");
+		System.out.printf("Expected: %s\n", expected);
+		System.out.printf("Got:      %s\n", result);
+		System.out.printf("= = = = = = = = = = = =\n\n");
+	}
+
+
+}

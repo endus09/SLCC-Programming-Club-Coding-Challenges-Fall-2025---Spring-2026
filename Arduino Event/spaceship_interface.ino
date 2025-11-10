cbff1d2d0dee2de8997ff20c5cc96ef5f7252318
endus <137748797+endus09@users.noreply.github.com>
2025-11-10 12:45:58 -0700

    Arduino Event
    Arduino Event

---
+++ Arduino Event/spaceship_interface.ino
@@ -0,0 +1,31 @@
+int switch_state = 0;
+
+void setup() {
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+  switch_state = digitalRead(2);
+
+  if (switch_state == LOW)
+    {
+      digitalWrite(3, HIGH);
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
+    }
+    else
+    {
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, HIGH);
+
+      delay(1000);
+
+      digitalWrite(4, HIGH);
+      digitalWrite(5, LOW);
+      delay(1000);
+    }
+}

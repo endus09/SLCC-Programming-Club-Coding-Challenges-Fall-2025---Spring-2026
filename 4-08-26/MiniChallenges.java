b42af33830c37c1bc51dbeba29dfeb10ead5f136
Ethan J. Hadley <96396138+Ethanscharlie@users.noreply.github.com>
2026-04-08 12:30:42 -0600

    Add files via upload

---
+++ 4-08-26/MiniChallenges.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class MiniChallenges {
+  // loop from 0 to n
+  public static void fizzbuzz(int n) {}
+
+  // given a list of words, return the average word length
+  public static double averageWordLength(List<String> words) {
+    return 0;
+  }
+
+  // return the multiples of the parameter "multiples_of" until "until"+1 has been reached
+  public static List<Integer> multiples(int multiplesOf, int until) {
+    return new ArrayList<>();
+  }
+
+  // create a text file, read from it here, print the text in it
+  public static void readThisFile() {}
+
+  // return the nth fibonacci number
+  public static int fibonacci(int n) {
+    return 0;
+  }
+
+  // given a list of words, print each word from the list in a frame / box
+  public static void printStringInFrame(List<String> words) {}
+
+  // given a sentence string, separate the words into a list and return
+  public static List<String> storeWordsFromSentence(String sentence) {
+    return new ArrayList<>();
+  }
+
+  // given a sentence string, find how many periods there are and return the amount
+  public static int findPeriodsInSentence(String sentence) {
+    return 0;
+  }
+
+  // given a sentence string, return every five-letter word however you see fit
+  public static String printEveryFiveLetterWord(String sentence) {
+    return "";
+  }
+
+  public static void main(String[] args) {
+    // Uncomment and test when ready
+
+    // fizzbuzz(100);
+
+    // System.out.println(averageWordLength(Arrays.asList("Hello", "World")));
+
+    // System.out.println(multiples(5, 25));
+
+    // readThisFile();
+
+    // System.out.println(fibonacci(9));
+
+    // printStringInFrame(Arrays.asList("Hello", "world", "in", "frame"));
+
+    // System.out.println(storeWordsFromSentence("Example sentence here"));
+
+    // System.out.println(findPeriodsInSentence("www.google.com"));
+
+    // System.out.println(printEveryFiveLetterWord("water plus melon maybe arent after"));
+  }
+}

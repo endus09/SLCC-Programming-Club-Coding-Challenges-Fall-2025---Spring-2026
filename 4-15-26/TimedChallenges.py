b8732fe3d2a5d5acf7c1ab362d7c884e022e3ff6
WhisperKnight <61612853+Bryson-C@users.noreply.github.com>
2026-04-15 13:03:23 -0600

    Update TimedChallenges.py

--- 4-15-26/TimedChallenges.py
+++ 4-15-26/TimedChallenges.py
@@ -26,37 +26,11 @@ def merge_sorted_lists(list_a, list_b):
 #print(f"{list5} and {list6} = [] got {merge_sorted_lists(list5,list6)}")
 
 
-
-
-  
-# Level 1 ERASE SOLUTION PLZ AFTER 
-# Pascal could be a 2-step problem, one to make the lists, and the other to print the triangle 
 def generate_pascals_triangle(n):
     triangle = []
-
-    for i in range(n):
-        # Start each row with 1
-        row = [1] * (i + 1)
-
-        # Calculate the middle values
-        for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-
-        triangle.append(row)
-
     return triangle
 
-# Level 2 ERASE SOLUTION PLZ AFTER
-def print_triangle(triangle):
-    n = len(triangle)
-    for i, row in enumerate(triangle):
-        # Create centering padding
-        print(" " * (n - i), end="")
-        print(*row)
+print(f"{generate_pascals_triangle(3)[0]} == [0]")
+print(f"{generate_pascals_triangle(3)[1]} == [1, 1]")
 
-
-# Usage to see potential solutions
-#rows = 5
-#my_triangle = generate_pascals_triangle(rows)
-#print(my_triangle)
-#print_triangle(my_triangle)
+print(f"{generate_pascals_triangle(3)[2]} == [1, 2, 1]")
